f995ebc5fcfc1d78d4ad14c63f1b2ad975b598b2
roy <89403308+itsaruproy@users.noreply.github.com>
2022-10-18 21:05:41 +0530

    feat: add chakra UI

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Box, Text, Button, HStack } from '@chakra-ui/react'
+const Header = () => {
+    return (
+        <Box
+            w={'100%'}
+            display={'flex'}
+            bg={'gray.200'}
+            p={'16px'}
+            textColor={'gray.700'}
+        >
+            <Box
+                px={'12'}
+                w={'100%'}
+                display="flex"
+                justifyContent={'space-between'}
+                alignItems={'center'}
+            >
+                <Box>
+                    <Text fontSize={'2xl'} fontWeight={'semibold'}>
+                        DriveShare
+                    </Text>
+                </Box>
+                <HStack spacing={'5'}>
+                    <Button>Log in</Button>
+                    <Button>Learn More</Button>
+                </HStack>
+            </Box>
+        </Box>
+    )
+}
+
+export default Header
---
+++ src/components/Lists.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Box, Flex, Text, Button, HStack } from '@chakra-ui/react'
+import { useToast } from '@chakra-ui/toast'
+
+const List = () => {
+    const toast = useToast()
+    const onClickHandler = () => {
+        toast({
+            title: 'Link copied to Clipboard',
+            status: 'success',
+            duration: 2000,
+            isClosable: true,
+        })
+    }
+
+    const onRevokeHandler = () => {
+        toast({
+            title: 'Access Revoked Successfully',
+            status: 'success',
+            duration: 2000,
+            isClosable: true,
+        })
+    }
+
+    return (
+        <Box p="16px" w={'320px'} bg="gray.50" shadow={'lg'} rounded={'md'}>
+            <Text mb={'10px'}>The Upload target description</Text>
+            <HStack>
+                <Button onClick={onRevokeHandler} colorScheme={'red'}>
+                    Revoke Access
+                </Button>
+                <Button onClick={onClickHandler} colorScheme={'purple'}>
+                    Copy Link
+                </Button>
+            </HStack>
+        </Box>
+    )
+}
+
+const Lists = () => {
+    return (
+        <Box w={'full'} display="flex" justifyContent={'center'}>
+            <Flex gap={'12px'} direction={'column'}>
+                <List />
+                <List />
+                <List />
+                <List />
+            </Flex>
+        </Box>
+    )
+}
+
+export default Lists
