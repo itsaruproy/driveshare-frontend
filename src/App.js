fa0dc576b826bc7b163a800432c4112cb1887756
roy <89403308+itsaruproy@users.noreply.github.com>
2022-10-18 22:30:32 +0530

    feat: Add guest home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Box } from '@chakra-ui/react'
 import Lists from './components/Lists'
 import Header from './components/Header'
+import Guest from './components/Guest'
 
 const App = () => {
     return (
         <Box bg={'gray.200'} minH="100vh">
             <Header />
-            <Lists />
+            {/* <Lists /> */}
+            <Guest />
         </Box>
     )
 }
---
+++ src/components/Guest.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import {
+    HStack,
+    Box,
+    Heading,
+    Text,
+    Button,
+    Link as ChakraLink,
+} from '@chakra-ui/react'
+import { CheckCircleIcon } from '@chakra-ui/icons'
+
+const Features = props => {
+    return (
+        <HStack>
+            <CheckCircleIcon />
+            <Text>{props.children}</Text>
+        </HStack>
+    )
+}
+
+const Guest = () => {
+    return (
+        <Box
+            w={'full'}
+            display={'flex'}
+            flexDirection={'column'}
+            alignItems={'center'}
+            gap={'12px'}
+        >
+            <Heading>Welcome to DriveShare</Heading>
+            <Box mt={'10'} display={'flex'} flexDirection={'column'}>
+                <Features>
+                    Give access to specific folders of your drive without
+                    sharing your drive
+                </Features>
+                <Features>
+                    You get an unique link of your upload target from the app,
+                    people can upload directly by visiting that link
+                </Features>
+                <Features>
+                    You can revoke the access of you Google Drive folder sharing
+                    anytime just in one click
+                </Features>
+            </Box>
+            <Box>
+                <ChakraLink
+                    href="https://www.youtube.com/watch?v=_X7lGKZrQKA&list=RDDrs55HB_fo0&index=9"
+                    isExternal
+                    textDecorationLine={'none'}
+                    style={{ textDecoration: 'none' }}
+                >
+                    <Button colorScheme={'purple'}>Demo Video</Button>
+                </ChakraLink>
+            </Box>
+        </Box>
+    )
+}
+
+export default Guest
