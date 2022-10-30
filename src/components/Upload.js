338a699f882537b168b544088a55bcc459dd861e
roy <89403308+itsaruproy@users.noreply.github.com>
2022-10-30 13:39:56 +0530

    fix: Upload UI changes

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,11 +1,28 @@
-import { Box, Button, Heading, Input, Text } from '@chakra-ui/react'
-import React, { useRef, useState } from 'react'
+import { Box, Button, Heading, Input, Text, useToast } from '@chakra-ui/react'
+import React, { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
 import { BASE_URL } from '../constants'
 
 const Upload = props => {
+    const toast = useToast()
     const uploadBtn = useRef(null)
     const [file, setFile] = useState(null)
+    const [target, setTarget] = useState('Example Target')
+    const [isAvailable, setIsAvailable] = useState(false)
+
+    useEffect(() => {
+        let linkID = props.match.params.id
+        const getUploadInfo = async () => {
+            try {
+                const response = await axios.get(BASE_URL + `/upload/${linkID}`)
+                console.log('Upload Info', response.data)
+                setTarget(response.data.info.targetName)
+                setIsAvailable(true)
+            } catch {}
+        }
+
+        getUploadInfo()
+    }, [props.match.params.id, setTarget, setIsAvailable])
 
     const onClickHandler = () => {
         uploadBtn.current.click()
@@ -25,12 +42,35 @@ const Upload = props => {
         let linkID = props.match.params.id
 
         try {
+            toast({
+                title: 'Uploading',
+                status: 'info',
+                isClosable: true,
+            })
+
             await axios.post(BASE_URL + `/upload/${linkID}`, formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
             })
-        } catch {}
+
+            toast.closeAll()
+            toast({
+                title: 'File uploaded successfully',
+                status: 'success',
+                duration: 4000,
+                isClosable: true,
+            })
+
+            setFile(null)
+        } catch {
+            toast({
+                title: 'Something went wrong',
+                status: 'error',
+                duration: 4000,
+                isClosable: true,
+            })
+        }
 
         if (file) {
             console.log(file)
@@ -46,9 +86,7 @@ const Upload = props => {
                 flexDirection={'column'}
                 gap={'16px'}
             >
-                <Heading mb={'20px'}>
-                    Upload file for Electronics Assignment
-                </Heading>
+                <Heading mb={'20px'}>{target}</Heading>
                 <Input
                     onChange={onChangeHandler}
                     hidden
