cd12fcdfa82086e7feeb86f03db7ddad19ff86fe
roy <89403308+itsaruproy@users.noreply.github.com>
2023-01-14 18:05:47 +0530

    add env

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5000'
+export const BASE_URL = process.env.BASE_URL || 'http://localhost:5000'
 export const SCOPES = 'https://www.googleapis.com/auth/drive'
 export const CLIENT_ID =
     '456086651360-r6v9clrue5ri8jhrlpm5it8okh2nttl5.apps.googleusercontent.com'
