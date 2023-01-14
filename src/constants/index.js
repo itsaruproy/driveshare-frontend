010b28678a306e6555413142732990ff955d82c5
roy <89403308+itsaruproy@users.noreply.github.com>
2023-01-14 18:26:40 +0530

    fix: trailing slash

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const BASE_URL =
-    process.env.BASE_URL || 'https://driveshare-backend.onrender.com/'
+    process.env.BASE_URL || 'https://driveshare-backend.onrender.com'
 export const SCOPES = 'https://www.googleapis.com/auth/drive'
 export const CLIENT_ID =
     '456086651360-r6v9clrue5ri8jhrlpm5it8okh2nttl5.apps.googleusercontent.com'
