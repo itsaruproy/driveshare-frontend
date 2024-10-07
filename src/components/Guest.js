050f7802f05360d998c6d5b5d248c08049718d20
Arup Roy <89403308+itsaruproy@users.noreply.github.com>
2024-10-07 23:52:08 +0530

    Update Guest.js

--- src/components/Guest.js
+++ src/components/Guest.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react'
 import {
     HStack,
@@ -50,6 +51,56 @@ const Guest = () => {
                 >
                     <Button colorScheme={'purple'}>Demo Video</Button>
                 </ChakraLink>
+            </Box>
+        </Box>
+    )
+}
+
+export default Guest
+*/
+import React from 'react'
+import {
+    HStack,
+    Box,
+    Heading,
+    Text,
+    Button,
+    Link as ChakraLink,
+} from '@chakra-ui/react'
+import { CheckCircleIcon } from '@chakra-ui/icons'
+
+const Features = props => {
+    return (
+        <HStack>
+            <CheckCircleIcon />
+            <Text>{props.children}</Text>
+        </HStack>
+    )
+}
+
+const Guest = () => {
+    return (
+        <Box
+            w={'full'}
+            display={'flex'}
+            flexDirection={'column'}
+            alignItems={'center'}
+            gap={'12px'}
+        >
+            <Heading>Welcome to DriveShare</Heading>
+            <Box mt={'10'} display={'flex'} flexDirection={'column'}>
+                <Features>
+                    Give access to specific folders of your drive without
+                    sharing your drive
+                </Features>
+                <Features>
+                    You get an unique link of your upload target from the app,
+                    people can upload directly by visiting that link
+                </Features>
+                <Features>
+                    You can revoke the access of you Google Drive folder sharing
+                    anytime just in one click
+                </Features>
             </Box>
         </Box>
     )
