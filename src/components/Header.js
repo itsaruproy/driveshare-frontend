004dc1e33162f536289c9230637456ea6b878e97
Arup Roy <89403308+itsaruproy@users.noreply.github.com>
2024-10-07 23:48:30 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react'
 import { useGoogleLogin } from '@react-oauth/google'
 import { connect } from 'react-redux'
@@ -72,18 +73,96 @@ const Header = props => {
                             Log in
                         </Button>
                     )}
-                    // <ChakraLink
-                    //     isExternal
-                    //     href="https://www.github.com/itsaruproy"
-                    //     style={{ textDecoration: 'none' }}
-                    // >
-                    //     <Button colorScheme={'blackAlpha'}>
-                    //         <HStack>
-                    //             <Icon as={FaGithub} />
-                    //             <Text>@itsaruproy</Text>
-                    //         </HStack>
-                    //     </Button>
-                    // </ChakraLink>
+                    <ChakraLink
+                        isExternal
+                        href="https://www.github.com/itsaruproy"
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <Button colorScheme={'blackAlpha'}>
+                            <HStack>
+                                <Icon as={FaGithub} />
+                                <Text>@itsaruproy</Text>
+                            </HStack>
+                        </Button>
+                    </ChakraLink>
+                </HStack>
+            </Box>
+        </Box>
+    )
+}
+
+const mapStateToProps = state => {
+    return { Token: state.auth.Token }
+}
+
+export default connect(mapStateToProps, { signIn, signOut })(Header)
+*/
+
+import React from 'react'
+import { useGoogleLogin } from '@react-oauth/google'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import { signIn, signOut } from '../actions'
+import { BASE_URL, SCOPES } from '../constants'
+import {
+    Box,
+    Text,
+    Button,
+    HStack,
+} from '@chakra-ui/react'
+
+const Header = props => {
+    const login = useGoogleLogin({
+        flow: 'auth-code',
+        scope: SCOPES,
+        onSuccess: async codeResponse => {
+            try {
+                const response = await axios.post(BASE_URL + '/getToken', {
+                    code: codeResponse.code,
+                })
+                props.signIn(response.data.token)
+                console.log(response)
+            } catch (err) {
+                console.log(err)
+            }
+        },
+        onError: errorResponse => console.log(errorResponse),
+    })
+
+    const logout = () => {
+        props.signOut()
+    }
+
+    return (
+        <Box
+            w={'100%'}
+            display={'flex'}
+            bg={'gray.200'}
+            p={'16px'}
+            textColor={'gray.700'}
+        >
+            <Box
+                px={'12'}
+                w={'100%'}
+                display="flex"
+                justifyContent={'space-between'}
+                alignItems={'center'}
+            >
+                <Box>
+                    <Text fontSize={'2xl'} fontWeight={'semibold'}>
+                        DriveShare
+                    </Text>
+                </Box>
+                <HStack spacing={'5'}>
+                    {props.Token ? (
+                        <Button onClick={logout} colorScheme={'teal'}>
+                            Log out
+                        </Button>
+                    ) : (
+                        <Button onClick={login} colorScheme={'teal'}>
+                            Log in
+                        </Button>
+                    )}
                 </HStack>
             </Box>
         </Box>
